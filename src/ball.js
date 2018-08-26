9e08423f4c4b6a1d93fa7386d07c224cc64cf431
xingobar <gary840227@gmail.com>
2018-08-26 16:01:57 +0700

    1.brick collision detect

--- src/ball.js
+++ src/ball.js
@@ -29,22 +29,39 @@ var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
 	bricks[c] = [];
 	for (var r = 0; r < brickRowCount; r++) {
-		bricks[c][r] = { x: 0, y: 0 };
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+function bricksCollision() {
+	for (var c = 0; c < brickColumnCount; c++) {
+		for (var r = 0; r < brickRowCount; r++) {
+			var b = bricks[c][r];
+			if (b.status) {
+				if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+					// collision bricks
+					b.status = 0;
+					dy = -dy;
+				}
+			}
+		}
 	}
 }
 
 function drawBricks() {
 	for (var c = 0; c < brickColumnCount; c++) {
 		for (var r = 0; r < brickRowCount; r++) {
-			var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-			var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = '#0095DD';
-			ctx.fill();
-			ctx.closePath();
+			if (bricks[c][r].status) {
+				var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+				var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = '#0095DD';
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 }
@@ -90,6 +107,7 @@ function draw() {
 	drawBricks();
 	drawerBall();
 	drawPaddle();
+	bricksCollision();
 
 	// 因為碰撞偵測位於球的中心，因此得減掉半徑
 	if (y + dy < ballRadius) {
