67cbcedd42d95718e9ff7f4eeefc4f62e0022d63
xingobar <gary840227@gmail.com>
2018-08-26 15:44:37 +0700

    1.add bricks

--- src/ball.js
+++ src/ball.js
@@ -14,8 +14,40 @@ var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
 document.addEventListener('keydown', keydownHandler, false);
 document.addEventListener('keyup', keyupHandler, false);
+
+var bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+	bricks[c] = [];
+	for (var r = 0; r < brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0 };
+	}
+}
+
+function drawBricks() {
+	for (var c = 0; c < brickColumnCount; c++) {
+		for (var r = 0; r < brickRowCount; r++) {
+			var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+			var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = '#0095DD';
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+}
 
 function drawerBall() {
 	ctx.beginPath();
@@ -55,6 +87,7 @@ function keydownHandler(e) {
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBricks();
 	drawerBall();
 	drawPaddle();
 
@@ -66,8 +99,8 @@ function draw() {
 		if (x > paddleX && x < paddleX + paddleWidth) {
 			dy = -dy;
 		} else {
-			alert('game over');
-			document.location.reload();
+			//alert('game over');
+			//document.location.reload();
 		}
 	}
 
