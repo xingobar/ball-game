29c06f5b08d9b052796c16ab1b4efcd3d582d682
xingobar <gary840227@gmail.com>
2018-08-26 14:57:07 +0700

    first commit

---
+++ src/ball.js
@@ -0,0 +1,21 @@
+var canvas = document.getElementById('ball_canvas');
+var ctx = canvas.getContext('2d');
+
+//畫正方形
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = '#FF0000';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+ctx.fillStyle = 'green';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 50, 50);
+ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+ctx.stroke();
+ctx.closePath();
